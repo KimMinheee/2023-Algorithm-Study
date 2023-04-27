a0f15ca91ec338e5bec3972f401c79e19d532144
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-04-27 17:24:15 +0900

    Update BOJ_1874.java

--- jaehwan/ch3_5스택과큐/BOJ_1874.java
+++ jaehwan/ch3_5스택과큐/BOJ_1874.java
@@ -15,12 +15,12 @@ public class boj_1874 {
         for(int i=0; i<num; i++){
             int A = Integer.parseInt(br.readLine());
             for(; j <= A; j++){
-                stack.push(j);
-                sb.append("+").append("\n");
+                stack.push(j); //1부터 들어온 값이 나올때까지 push
+                sb.append("+\n");
             }
             if(stack.peek()==A){
                 stack.pop();
-                sb.append("-").append("\n");
+                sb.append("-\n");
             }
             else{
                 yesNo=true;
@@ -35,3 +35,8 @@ public class boj_1874 {
         br.close();
     }
 }
+/*
+풀이 
+처음 나온 숫자까지 push 그숫자가 나왔으면 pop 그다음 숫자까지 큰지 작은지 판다내서 push pop 숫자가 작아서 pop가 필요하면
+pop하면서 숫자꺼내줌 찾으면 그것도 팝 
+*/

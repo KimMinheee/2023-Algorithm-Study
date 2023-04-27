eadf4d94af9711a2a754757762e88a45131985c8
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-04-27 17:41:13 +0900

    Update BOJ_1874.java

--- jaehwan/ch3_5스택과큐/BOJ_1874.java
+++ jaehwan/ch3_5스택과큐/BOJ_1874.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class boj_1874 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
@@ -23,19 +23,20 @@ public class boj_1874 {
                 sb.append("-\n");
             }
             else{
-                yesNo=true; //안되는 경우의 수 있으면 플래그 세워둠
+                yesNo=true;
+                System.out.println("NO");
+                break;
             }
         }
-        if(yesNo==true){
-            System.out.println("NO");
-        }
-        else{
+        if(yesNo!=true){
             System.out.println(sb);
         }
+       
         br.close();
     }
 }
 /*
+문제 해설 주어진 수열을 팝을 이용해서 그 수열을 똑같이 만들어낼수있는가?
 풀이 
 처음 나온 숫자까지 push 그숫자가 나왔으면 pop 그다음 숫자까지 큰지 작은지 판다내서 push pop 숫자가 작아서 pop가 필요하면
 pop하면서 숫자꺼내줌 찾으면 그것도 팝 

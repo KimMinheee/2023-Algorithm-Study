862677b1484905f7e717c836c1ccfaba81746f7b
Park, Sojeong <creerari01@gmail.com>
2023-05-12 18:17:04 +0900

    [BOJ-1260] edited dfs,bfs sojeong solved

--- sojeong/ch05_1_탐색/BOJ_11724_연결요소의개수구하기.java
+++ sojeong/ch05_1_탐색/BOJ_11724_연결요소의개수구하기.java
@@ -3,52 +3,52 @@ package ch05_1_탐색;
 import java.util.Scanner;
 
 public class BOJ_11724_연결요소의개수구하기 {
-    static int[][] graph = new int[1001][1001];
-    static int V;
-    static int E;
-    static boolean[] visited = new boolean[1001];
 
-    public static void dfs(int index) {
-        if(visited[index] == true)
-            return;
-        else {
-            visited[index] = true;
-            for(int i = 1; i <= V; i++) {
-                if(graph[index][i] == 1) {
-                    dfs(i);
-                }
-            }
-        }
-    }
+    static int N; // 정점의 개수
+    static int M; // 간선의 개수
+    static int[][] adj; // 인접행렬
+
+    static boolean[] visit; // 방문 체크
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); // 정점의 개수
+        M = sc.nextInt(); // 간선의 개수
 
-        V = sc.nextInt();
-        E = sc.nextInt();
+        adj = new int[N + 1][N + 1];
 
-        int a,b;
-        for(int i = 0; i < E; i++) {
-            a = sc.nextInt();
-            b = sc.nextInt();
-
-            // 간선 연결
-            graph[a][b] = graph[b][a] = 1;
+        //인접행렬 입력
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            adj[a][b] = 1;
+            adj[b][a] = 1;
         }
 
-        int result = 0 ;
+        visit = new boolean[N + 1];
 
-        // dfs 탐색
-        for(int i = 1; i <= V; i++) {
-            if(visited[i] == false) { // 방문한 적 없는 노드라면 dfs.
+        int count = 0; // 연결요소 개수
+
+        //방문 배열 돌면서 방문하지 않은 정점이 있으면 탐색하고 연결요소 개수 1 증가
+        for (int i = 1; i < N + 1; i++) {
+            if (!visit[i]) {
                 dfs(i);
-                result++;
+                count++;
             }
         }
 
-        System.out.println(result);
+        System.out.println(count);
+    }
 
-        sc.close();
-        return;
+    public static void dfs(int start) {
+        //방문했다.
+        visit[start] = true;
+
+        //방문 배열 처음부터 들면서 간선이 연결되어 있고, 방문을 하지 않았으면 dfs 탐색 ㄱㄱ
+        for (int i = 1; i <= N; i++) {
+            if (adj[start][i] == 1 && !visit[i]) {
+                dfs(i);
+            }
+        }
     }
 }

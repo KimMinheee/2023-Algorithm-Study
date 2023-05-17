2a854df63b21390d5017b72eef9354ce6934f6f2
Serendipity-Song <thdalwh3867@gmail.com>
2023-05-17 14:47:40 +0900

    [BOJ-1920] 원하는 정수 찾기 heain solved

--- haein/BOJ_1920.java
+++ haein/BOJ_1920.java
@@ -2,35 +2,57 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.Arrays;
 
 public class Main {
+
+//    값 입력받기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        TreeSet<Integer> treeSet = new TreeSet<>();
-
         for (int i = 0; i < N; i++) {
-            treeSet.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
 
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
             int x = Integer.parseInt(st.nextToken());
-            if (treeSet.contains(x)) {
+            if (binarySearch(arr, x)) {
+//                 찾았을 경우, 1입력
                 sb.append("1\n");
             } else {
+//                0입력
                 sb.append("0\n");
             }
         }
 
         System.out.print(sb.toString());
+    }
+
+//    이진탐색
+    public static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
     }
 }

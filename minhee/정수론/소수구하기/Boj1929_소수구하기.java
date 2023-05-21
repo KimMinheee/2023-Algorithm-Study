20c60f02b8338b5db927abcf7569069080ca3b70
KimMinheee <99alsgmlalsl@naver.com>
2023-05-21 23:55:00 +0900

    [BOJ-1929] 소수구하기 minhee solving - 에라토스테네스 체 사용 / 88% 오류

---
+++ minhee/정수론/소수구하기/Boj1929_소수구하기.java
@@ -0,0 +1,47 @@
+package minhee.정수론.소수구하기;
+
+/**
+ * BOJ1929 - 소수구하기
+ * : 에라토스테네스 체 사용
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Boj1929_소수구하기 {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] nums = getPrimeNumber(M, N);
+        for(int i=M; i<=N; i++){
+            if(nums[i] == 0) bw.write(i+"\n");
+        }
+        bw.flush();
+        br.close();
+        br.close();
+    }
+    static int[] getPrimeNumber(int start, int end){
+        int[] nums = new int[end+1]; //N+1개
+
+        //소수가 아닌 수들을 1로 변경
+        for(int i=2; i<=end; i++){
+            if(nums[i] == 0){
+                int tmp = i;
+                int multiply = 2;
+                while(tmp * multiply <= end){
+                    nums[tmp * multiply] = 1;
+                    multiply++;
+                }
+            }
+            else continue;
+        }
+
+        return nums;
+
+    }
+}

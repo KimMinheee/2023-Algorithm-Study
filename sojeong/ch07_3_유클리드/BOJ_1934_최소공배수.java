ce09edf039a3e6769e3db7af487ce75e68a38481
Sojeong Park <creerari01@gmail.com>
2023-05-22 21:26:29 +0900

    [BOJ_11689] 오일러피 sj solved

--- sojeong/ch07_3_유클리드/BOJ_1934_최소공배수.java
+++ sojeong/ch07_3_유클리드/BOJ_1934_최소공배수.java
@@ -1,13 +1,16 @@
 package ch07_3_유클리드;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_1934_최소공배수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
 
         for(int i=0;i<T;i++){
@@ -15,11 +18,15 @@ public class BOJ_1934_최소공배수 {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
             int ans = N*M/gcd(N,M);
-            System.out.println(ans);
+            bw.write(Integer.toString(ans));
+            bw.newLine();
         }
+        bw.flush();
+        bw.close();
+
     }
     private static int gcd(int N, int M) {
-        if(M == 0){
+        if(M == 0){ // 계속 나누다가 0이 되면 return
             return N;
         }
         else{

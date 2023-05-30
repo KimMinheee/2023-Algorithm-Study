dd3b2cc91666a0a89c449447eead1d1915afe0a4
KimMinheee <99alsgmlalsl@naver.com>
2023-05-30 23:52:15 +0900

    [BOJ-1325] 효율적인 해킹 minhee solving - bfs - 시간초과 발생

--- minhee/그래프/그래프의표현/Boj1325_효율적인해킹.java
+++ minhee/그래프/그래프의표현/Boj1325_효율적인해킹.java
@@ -1,42 +1,58 @@
 package minhee.그래프.그래프의표현;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Boj1325_효율적인해킹 {
-    static int N,M;
-    static List<List<Integer>> computers = new ArrayList<>();
+/**
+ * [DFS/BFS]p1325_효율적인해킹
+ * 아이디어:
+ * - 오->왼 방향으로 인접리스트에 값 넣기
+ * - visited,결과 배열(연결된 컴 수 저장)
+ */
+class Boj1325_효율적인해킹 {
+    static int N,M; //컴수,신뢰관계수
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     static int[] answer;
-    public static void main(String[] args)throws IOException {
+    static boolean[] visited;
+    static int cnt=0;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        answer = new int[N+1]; //정답 개수 저장
-        for(int i=0; i<=N; i++){ //N+1개
-            computers.add(new ArrayList<>());
+        //초기화
+        answer = new int[N+1];
+        //visited = new boolean[N+1];
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<Integer>());
         }
 
-        //신뢰 관계 연결
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            connect(A,B);
+            int slave = Integer.parseInt(st.nextToken());
+            int master = Integer.parseInt(st.nextToken());
+            list.get(master).add(slave);
         }
 
-        boolean[] visit = new boolean[N+1];
-        for(int i=1; i<=N; i++){
-            answer[i] = checkTrust(i, 0, visit);
-            Arrays.fill(visit,false);
+        //1~n 경우
+        for(int a = 1; a<=N; a++){
+            visited = new boolean[N+1];
+            bfs(a);
+            answer[a] = cnt;
+            max = Math.max(max,cnt);
+            cnt=0;
         }
 
-        int max = Arrays.stream(answer).max().getAsInt();
+        StringBuilder sb = new StringBuilder();
         for(int i=1; i<=N; i++){
-            if(answer[i] == max) sb.append(i).append(" ");
+            if(answer[i]==max) sb.append(i+" ");
         }
 
         bw.write(sb.toString());
@@ -44,17 +60,24 @@ public class Boj1325_효율적인해킹 {
         bw.close();
         br.close();
     }
-    static void connect(int A, int B){
-        computers.get(B).add(A);
-    }
-    static int checkTrust(int com, int cnt, boolean[] visit){
-        int tmp = cnt;
-        for(int trust : computers.get(com)){
-            if(!visit[trust]){
-                visit[trust] = true;
-                tmp = checkTrust(trust,cnt+1,visit);
+
+    static void bfs(int start){
+        visited[start] = true;
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+
+        while(!que.isEmpty()){
+            int master = que.poll();
+            for(int i=0; i<list.get(master).size();i++){
+                int tmp = list.get(master).get(i);
+                if(visited[tmp]==false){
+                    cnt++;
+                    visited[tmp] = true;
+                    que.add(tmp);
+                }
             }
         }
-        return tmp;
     }
 }
+
+

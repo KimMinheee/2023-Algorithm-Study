9d2bb6c3397f0aa51347f62e5deede6e42324aa2
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-06-03 14:43:01 +0900

    [BOJ_21568]Ax+By=C jaehwan solved - gcd 사용

--- jaehwan/ch7_3_유클리드호제법/BOJ_21568.java
+++ jaehwan/ch7_3_유클리드호제법/BOJ_21568.java
@@ -30,9 +30,9 @@ public class BOJ_21568 {  //Ax+By = C
             return ret;
         }
         long q = a/b;
-        long[] v = Excute(b, a%b); //재귀
+        long[] v = Excute(b, a%b); //재귀 여기서 계산 걸림 b
         ret[0] = v[1]; //역순으로 올라오면서 x,y값을 계산
-        ret[1] = v[0] - v[1]*q; //몫을 게산하는 역산 로직
+        ret[1] = v[0] - v[1]*q; //몫을 게산하는 역산 로직 x=이전 y, y=이전 x -이전 y *몫
         return ret; //재귀에서 빠져나오는 영역에서 실행하면 자연스럽게 역순이 됨
     }
     private static long gcd(long a, long b) {

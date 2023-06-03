d7ee8505ce28e7e18c8779956bd107b7afb5514f
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-06-03 15:40:00 +0900

    [BOJ_21568]AxBx jaehwan solved - gcd 확장 사용

--- jaehwan/ch7_3_유클리드호제법/BOJ_21568.java
+++ jaehwan/ch7_3_유클리드호제법/BOJ_21568.java
@@ -7,17 +7,16 @@ import java.util.StringTokenizer;
 public class BOJ_21568 {  //Ax+By = C
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        BufferedWriter bw  = new BufferedWriter( new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         long gcd = gcd(a,b); //최대공약수
-
-        if(c%gcd != 0) { System.out.println(-1); } //c가 최대공약수의 배수가 아닐경우 바로 끝냄
+        //Ax+By = C 는 c % gcd != 0 일 경우에만 정수해를 가짐 (문제에서 정수해를 요구함)
+        if(c % gcd != 0) { System.out.println(-1); } //c가 최대공약수의 배수가 아닐경우 바로 끝냄
         else {  //답이 있는 경우
-            int mok = (int) (c/gcd);
+            int mok = (int) (c / gcd);
             long[] ret = Excute(a, b); //재귀 함수 호출하는 유클리드 호제법 함수 호출
             System.out.println(ret[0]*mok + " " + ret[1]*mok); //결과값에 c/최대 공약수의 값을 곱한 후 해당 값을 출력하기
         }
@@ -31,17 +30,25 @@ public class BOJ_21568 {  //Ax+By = C
         }
         long q = a/b;
         long[] v = Excute(b, a%b); //재귀 여기서 계산 걸림 b
-        ret[0] = v[1]; //역순으로 올라오면서 x,y값을 계산
-        ret[1] = v[0] - v[1]*q; //몫을 게산하는 역산 로직 x=이전 y, y=이전 x -이전 y *몫
+        //몫을 계산하는 역산 로직 역순으로 올라오면서 x,y값을 계산
+        ret[0] = v[1];          //x = 이전 y
+        ret[1] = v[0] - v[1]*q; // y = 이전 x - 이전 y * 몫
         return ret; //재귀에서 빠져나오는 영역에서 실행하면 자연스럽게 역순이 됨
     }
     private static long gcd(long a, long b) {
-        while(b!= 0) {
-            long temp = a%b;
+        //반복문을 사용해서 비재귀적으로 나타냄
+        while(b!= 0) {  
+            long temp = a % b;
             a=b;
             b=temp;
         }
-        return Math.abs(a);
+        /*
+        초기값: a = 24, b = 36
+        1회 반복: temp = 24 % 36 = 24, a = 36, b = 24
+        2회 반복: temp = 36 % 24 = 12, a = 24, b = 12
+        3회 반복: temp = 24 % 12 = 0, a = 12, b = 0
+        */
+        return Math.abs(a); //절대값 반환 
     }
 }
 

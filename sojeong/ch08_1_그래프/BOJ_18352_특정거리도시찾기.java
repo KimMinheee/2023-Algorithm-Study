3ccf216bf0187f1c1e16095c3c34d9d513349b23
Sojeong Park <creerari01@gmail.com>
2023-06-06 01:40:15 +0900

    [BOJ_2251] 물통 sj solving

--- sojeong/ch08_1_그래프/BOJ_18352_특정거리도시찾기.java
+++ sojeong/ch08_1_그래프/BOJ_18352_특정거리도시찾기.java
@@ -29,10 +29,10 @@ public class BOJ_18352_특정거리도시찾기 {
         arr = new ArrayList[N + 1];
         ans = new ArrayList<Integer>();
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++) { // 연결리스트에 노드 추가
             arr[i] = new ArrayList<Integer>();
         }
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) { //단방향 도시 정보 입력받기(간선 정보 입력)
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken()); // A번 도시
             int B = Integer.parseInt(st.nextToken()); // B번 도시
@@ -40,22 +40,17 @@ public class BOJ_18352_특정거리도시찾기 {
                 System.out.println("A는 1이상의 자연수이고, B는 N이하의 자연수입니다.");
             } else {
                 arr[A].add(B);
-
             }
         }
-
         visited = new int[N + 1]; // 방문 배열 초기화
         for (
-            int i = 0;
-            i <= N; i++) {
+            int i = 0; i <= N; i++) {
             visited[i] = -1;
         }
 
         BFS(X);
-        for (
-            int i = 0;
-            i <= N; i++) {
-            if (visited[i] == K) {
+        for (int i = 0; i <= N; i++) {
+            if (visited[i] == K) { // K: 거리정보
                 ans.add(i);
             }
         }
@@ -64,8 +59,7 @@ public class BOJ_18352_특정거리도시찾기 {
         } else {
             Collections.sort(ans);
         }
-        for (
-            int temp : ans) {
+        for (int temp : ans) {
             System.out.println(temp);
         }
 
@@ -78,7 +72,7 @@ public class BOJ_18352_특정거리도시찾기 {
         while (!queue.isEmpty()) {
             int now_Node = queue.poll();
             for (int i : arr[now_Node]) {
-                if (visited[i] == -1) {
+                if (visited[i] == -1) { // 방문한 적이 없으면 방문하고 업데이트
                     visited[i] = visited[now_Node] + 1;
                     queue.add(i);
                 }

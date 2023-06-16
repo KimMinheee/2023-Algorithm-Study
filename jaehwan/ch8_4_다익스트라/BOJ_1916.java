b9530f5e5ec21636f91919a6373ef64a51afb936
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-06-16 22:55:42 +0900

    [BOJ_1916]최소비용 jaehwan solved - 다익스트라 사용

--- jaehwan/ch8_4_다익스트라/BOJ_1916.java
+++ jaehwan/ch8_4_다익스트라/BOJ_1916.java
@@ -17,6 +17,11 @@ class Node_1916 implements Comparable<Node_1916> {
     }
 
     @Override
+    //이 메서드는 노드들을 가중치를 기준으로 정렬하기 위해 사용
+    //현재 노드와 비교 대상인 o 노드의 가중치를 비교하여 값을 반환
+    //반환 값이 음수일 경우 현재 노드의 가중치가 작은 것으로 판단
+    //반환 값이 양수일 경우 현재 노드의 가중치가 큰 것으로 판단
+    //반환 값이 0일 경우 가중치가 같은 것으로 판단
     public int compareTo(Node_1916 o) {
         return value - o.value;
     }
@@ -58,7 +63,7 @@ public class BOJ_1916 {
     }
 
     public static int dStra(int start, int end) {
-        PriorityQueue<Node_1916> pq = new PriorityQueue<>();
+        PriorityQueue<Node_1916> pq = new PriorityQueue<>();//자동으로 거리가 최소인 노드를 선택할수 있는 자료구조
         pq.add(new Node_1916(start, 0));
         distance[start] = 0;//최단거리 배열
         while (!pq.isEmpty()) {

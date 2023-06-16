4629a788ea7e5b9fe658349438d437005ebcac6b
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-06-16 22:54:34 +0900

    [BOJ_1753]최단경로 jaehwan solved - 다익스트라 사용

--- jaehwan/ch8_4_다익스트라/BOJ_1753.java
+++ jaehwan/ch8_4_다익스트라/BOJ_1753.java
@@ -10,6 +10,11 @@ class Node implements Comparable<Node>{
     }
 
     @Override
+    //이 메서드는 노드들을 가중치를 기준으로 정렬하기 위해 사용
+    //현재 노드와 비교 대상인 o 노드의 가중치를 비교하여 값을 반환
+    //반환 값이 음수일 경우 현재 노드의 가중치가 작은 것으로 판단
+    //반환 값이 양수일 경우 현재 노드의 가중치가 큰 것으로 판단
+    //반환 값이 0일 경우 가중치가 같은 것으로 판단
     public int compareTo(Node o) {
         return weight - o.weight;
     }
@@ -62,6 +67,7 @@ public class BOJ_1753 {
     }
 
     private static void dijkstra(int start){
+        //자동으로 거리가 최소인 노드를 선택할수 있는 자료구조 정렬기준은 compareTo함수에서 설정
        PriorityQueue<Node> queue = new PriorityQueue<>();
        boolean[] check = new boolean[v + 1];
        queue.add(new Node(start, 0));

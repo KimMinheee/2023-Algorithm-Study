592dfd6d0b1d48d39185c59d9c9efefd3a577fcf
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-07-23 09:40:58 +0900

    [BOJ_10868]최솟값 찾기 jaehwan solved - 인덱스 트리 사용

--- jaehwan/ch9_4_세그먼트 트리/BOJ_10868.java
+++ jaehwan/ch9_4_세그먼트 트리/BOJ_10868.java
@@ -19,44 +19,45 @@ public class BOJ_10868 { //[BOJ_10868]최솟값 찾기 jaehwan solved - 인덱�
         //배열의 크기 = 2^(h+1) = 2^k*2
         //시작인덱스 = 2^k
         int treeSize= (int)Math.pow(2,treeH+1); //이진트리에서 높이가 h라면 필요한 배열의 크기는 2^(h+1) = 2^k*2
-        int NodeStartIndexLeft = treeSize / 2 - 1 ; //채워야하는 원본 리프노드 값 시작점
+        int NodeStartIndexLeft = treeSize / 2 - 1 ; //채워야하는 원본 리프노드 값 시작점예제 기준 15
         tree=new long[treeSize+1];
         for(int i=0;i<tree.length;i++){
             tree[i] = Integer.MAX_VALUE;
         }
+        //16부터 25까지
         for(int i=NodeStartIndexLeft+1; i <= NodeStartIndexLeft + N; i++){ //리프노드 입력
             tree[i]=Long.parseLong(br.readLine());
         }
         setTree(treeSize-1); //초기 트리 생성
-        for(int i=0;i<M;i++){
-            st=new StringTokenizer(br.readLine());
-            int s=Integer.parseInt(st.nextToken());
-            int e=Integer.parseInt(st.nextToken());
-            s= s+NodeStartIndexLeft;
-            e= e+NodeStartIndexLeft;
-            System.out.println(getMin(s,e));
+        for(int i=0;i<M;i++){ //질의 
+            st=new StringTokenizer(br.readLine());  // 1 10가 왔다 하면  
+            int s=Integer.parseInt(st.nextToken()); // 1
+            int e=Integer.parseInt(st.nextToken()); // 10
+            s= s+NodeStartIndexLeft;                // 16 
+            e= e+NodeStartIndexLeft;                // 25
+            System.out.println(getMin(s,e));        // 16 25
         }
         br.close();
     }
 
-    private static long getMin(int s, int e) {
-        long Min= Long.MAX_VALUE;
-        while(s<=e){
-            if(s%2==1){
-                Min=Math.min(Min,tree[s]);
+    private static long getMin(int s, int e) { //16 25
+        long Min= Long.MAX_VALUE; 
+        while(s <= e){ 
+            if(s%2==1){ //0 0 
+                Min=Math.min(Min,tree[s]); //[1] = 5 
                 s++;
             }
-            s=s/2;
-            if(e%2==0){
-                Min=Math.min(Min,tree[e]);
-                e--;
+            s=s/2; //8 4 2 1 1
+            if(e%2==0){ //1 0 0
+                Min=Math.min(Min,tree[e]); //[12] = 5
+                e--; // 11 1
             }
-            e=e/2;
+            e=e/2; //5 2 0
         }
-        return Min;
+        return Min; //5 
     }
 
-    private static void setTree(int i) {
+    private static void setTree(int i) { //31
         while(i!=1){ //인덱스가 루트가 아닐때까지
             if(tree[i/2] > tree[i]){ // 현재값과 비교해서 현재 값이 더 작을때
                 tree[i/2] = tree[i]; //해당 값을 부모노드에 저장하기

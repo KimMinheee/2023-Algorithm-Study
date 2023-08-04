60528e981577bc662fb0fba15446072bfb22fd6c
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-08-04 23:20:57 +0900

    [BOJ_14501]퇴사 jaehwan solved - dp 사용

--- jaehwan/ch11_1_동적계획법/BOJ_14501.java
+++ jaehwan/ch11_1_동적계획법/BOJ_14501.java
@@ -21,11 +21,12 @@ public class BOJ_14501 {//[BOJ_14501]퇴사 jaehwan solved - dp 사용
             P[i]=Integer.parseInt(st.nextToken());
         }
         for(int i=N;i>0;i--){
-            if(i+T[i] > N + 1){
-                D[i]=D[i+1];
+            if(i+T[i] > N + 1){//i번째 상담일을 퇴사일까지 끝낼 수 없을때
+                D[i]=D[i+1]; //i+1일 ~ 퇴사일에 벌 수 있는 최대 수입
             }
             else{
-                D[i]=Math.max(D[i+1], P[i]+D[i+T[i]]);
+                D[i]=Math.max(D[i+1], P[i]+D[i+T[i]]);//상담 가능 한경우 i+1일 ~ 퇴사일에 벌 수 있는 최대 수입 & i번째 상담 비용 
+                //+ i번째 상담이 끝난 다음날부터 퇴사일까지의 최대 수입 비교
             }
         }
         System.out.println(D[1]);//첫번째 날부터 세야하니까

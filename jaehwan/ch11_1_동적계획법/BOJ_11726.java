28a8f692aab64762515885a28ca629eb537e8745
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-08-08 14:50:23 +0900

    [BOJ_11726]타일채우기 jaehwan solved - dp사용

---
+++ jaehwan/ch11_1_동적계획법/BOJ_11726.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ_11726 {//[BOJ_11726]타일채우기 jaehwan solved - dp사용
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+
+        long[] D=new long[n+1];
+        D[0]=0;
+        D[1]=1;
+        D[2]=2;
+        for(int i=3;i<n+1;i++){
+            D[i]=D[i-1] + D[i-2];
+        }
+        System.out.println(D[n] % 10007);
+    }
+}
+//예제에서 주어진 9와 55 를 보고 D[2]를 직접 그려보고 피보나치 수열임을 발견했다.
